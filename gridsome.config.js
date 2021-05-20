7d4a86190b5f309349b141667324ca1cdb0b9ec5
aircrftdev <59378503+aircrftdev@users.noreply.github.com>
2021-05-19 21:13:04 -0700

    Commit

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Aircrft / blog',
+  siteName: 'Aircrft / vlog',
   siteDescription: 'Whats your moonshot?',
 
   templates: {
